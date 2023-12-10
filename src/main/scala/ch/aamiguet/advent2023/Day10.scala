1bb8892c5b48df561efd5812ba78f8be1ccf283d
Antoine Amiguet <amiguet.antoine@gmail.com>
2023-12-10 23:23:37 +0100

    Solving day 10 - part two

--- src/main/scala/ch/aamiguet/advent2023/Day10.scala
+++ src/main/scala/ch/aamiguet/advent2023/Day10.scala
@@ -53,14 +53,14 @@ object Day10 extends App:
     val y = lines.drop(x).head.indexWhere(_ == 'S')
     Pos(x, y)
 
-  private def findLoop(
-      sp: Pos,
+  private def findLoop0(
+      start: (Pos, Direction),
       pipes: Map[Pos, Pipe],
-      direction: Direction,
-      path: List[Pos]
-  ): List[Pos] =
-    val nextPos = path.head.nextPosition(direction)
-    if nextPos == sp then nextPos :: path
+      path: List[(Pos, Direction)]
+  ): List[(Pos, Direction)] =
+    val (pos, direction) = path.head
+    val nextPos = pos.nextPosition(direction)
+    if nextPos == start._1 then start :: path
     else
       val nextDirection =
         for
@@ -68,17 +68,18 @@ object Day10 extends App:
           nd <- pipe.nextDirection(direction)
         yield nd
       nextDirection match
-        case Some(d) => findLoop(sp, pipes, d, nextPos :: path)
+        case Some(d) => findLoop0(start, pipes, (nextPos, d) :: path)
         case None => Nil
 
   private def findLoop(
       sp: Pos,
       pipes: Map[Pos, Pipe],
       directions: List[Direction] = List(North, East, South, West)
-  ): List[Pos] =
+  ): List[(Pos, Direction)] =
     if directions.isEmpty then Nil
     else
-      val loop = findLoop(sp, pipes, directions.head, List(sp))
+      val start = (sp, directions.head)
+      val loop = findLoop0(start, pipes, List(start))
       if loop.nonEmpty then loop
       else findLoop(sp, pipes, directions.tail)
 
@@ -88,4 +89,79 @@ object Day10 extends App:
     val loop = findLoop(sp, pipes)
     loop.size / 2
 
+  private def leftDirections(entry: Direction, exit: Direction): List[Direction] =
+    (entry, exit) match
+      case (South, North) => List(West)
+      case (North, South) => List(East)
+      case (East, West) => List(South)
+      case (West, East) => List(North)
+      case (South, East) => List(West, North)
+      case (East, North) => List(South, West)
+      case (North, West) => List(East, South)
+      case (West, South) => List(North, East)
+      case _ => Nil
+
+  private def rightDirections(entry: Direction, exit: Direction): List[Direction] =
+    (entry, exit) match
+      case (South, North) => List(East)
+      case (North, South) => List(West)
+      case (East, West) => List(North)
+      case (West, East) => List(South)
+      case (South, West) => List(East, North)
+      case (West, North) => List(South, East)
+      case (North, East) => List(West, South)
+      case (East, South) => List(North, West)
+      case _ => Nil
+
+  private def switchDirection(direction: Direction) =
+    direction match
+      case North => South
+      case West => East
+      case South => North
+      case East => West
+
+  def part2(lines: List[String]): Int =
+    val allDirections = List(North, South, East, West)
+    val pipes = parse(lines)
+    val sp = startingPos(lines)
+    val loop = findLoop(sp, pipes)
+    val pathTiles = loop.map(_._1).toSet
+    val maxX = lines.size
+    val maxY = lines.head.size
+    val loopWithEntry = loop.zip(loop.tail).map((p1, p2) => (p1._1, switchDirection(p2._2), p1._2))
+    val (leftTiles, rightTiles) = loopWithEntry.foldLeft((Set.empty[Pos], Set.empty[Pos])):
+      (acc, b) =>
+        def addConnectedTiles0(pos: List[Pos], tiles: Set[Pos]): Set[Pos] =
+          if pos.isEmpty then tiles
+          else
+            val p = pos.head
+            val connectedPos =
+              allDirections.map(p.nextPosition(_))
+            val newPos = (pos.tail ++ connectedPos).filter(p =>
+              p.x >= 0 && p.x <= maxX && p.y >= 0 && p.y <= maxY && !tiles.contains(p) && !pathTiles
+                .contains(p)
+            )
+            addConnectedTiles0(newPos, tiles + p)
+
+        def addConnectedTiles(pos: Pos, directions: List[Direction], tiles: Set[Pos]): Set[Pos] =
+          val cands = directions
+            .map(pos.nextPosition(_))
+            .filter(p =>
+              p.x >= 0 && p.x <= maxX && p.y >= 0 && p.y <= maxY && !tiles.contains(p) && !pathTiles
+                .contains(p)
+            )
+          addConnectedTiles0(cands, tiles)
+
+        val (pos, entry, exit) = b
+        val (leftTiles, rightTiles) = acc
+        val lds = leftDirections(entry, exit)
+        val rds = rightDirections(entry, exit)
+        (addConnectedTiles(pos, lds, leftTiles), addConnectedTiles(pos, rds, rightTiles))
+
+    // assuming (0, 0) is not enclosed (and not on the path)
+    println(pathTiles.contains(Pos(0, 0)))
+    if leftTiles.contains(Pos(0, 0)) then rightTiles.size
+    else leftTiles.size
+
   println(part1(lines))
+  println(part2(lines))
--- src/test/scala/ch/aamiguet/advent2023/Day10Test.scala
+++ src/test/scala/ch/aamiguet/advent2023/Day10Test.scala
@@ -24,7 +24,65 @@ class Day10Test extends AnyFlatSpec with should.Matchers:
   val lines1 = input1.split("\n").toList
   val lines2 = input2.split("\n").toList
 
-  "Day10" should "solve all examples" in {
+  "Day10" should "solve both examples for part 1" in {
     assert(part1(lines1) == 4)
     assert(part1(lines2) == 8)
+  }
+
+  "Day10" should "solve first example for part 2" in {
+    val lines =
+      """...........
+        |.S-------7.
+        |.|F-----7|.
+        |.||.....||.
+        |.||.....||.
+        |.|L-7.F-J|.
+        |.|..|.|..|.
+        |.L--J.L--J.
+        |...........""".stripMargin.split("\n").toList
+    assert(part2(lines) == 4)
+  }
+
+  "Day10" should "solve first example variation for part 2" in {
+    val lines =
+      """..........
+        |.S------7.
+        |.|F----7|.
+        |.||....||.
+        |.||....||.
+        |.|L-7F-J|.
+        |.|..||..|.
+        |.L--JL--J.
+        |..........""".stripMargin.split("\n").toList
+    assert(part2(lines) == 4)
+  }
+
+  "Day10" should "solve large example for part 2" in {
+    val lines =
+      """.F----7F7F7F7F-7....
+        |.|F--7||||||||FJ....
+        |.||.FJ||||||||L7....
+        |FJL7L7LJLJ||LJ.L-7..
+        |L--J.L7...LJS7F-7L7.
+        |....F-J..F7FJ|L7L7L7
+        |....L7.F7||L7|.L7L7|
+        |.....|FJLJ|FJ|F7|.LJ
+        |....FJL-7.||.||||...
+        |....L---J.LJ.LJLJ...""".stripMargin.split("\n").toList
+    assert(part2(lines) == 8)
+  }
+
+  "Day10" should "solve large example with junk for part 2" in {
+    val lines =
+      """FF7FSF7F7F7F7F7F---7
+        |L|LJ||||||||||||F--J
+        |FL-7LJLJ||||||LJL-77
+        |F--JF--7||LJLJ7F7FJ-
+        |L---JF-JLJ.||-FJLJJ7
+        ||F|F-JF---7F7-L7L|7|
+        ||FFJF7L7F-JF7|JL---7
+        |7-L-JL7||F7|L7F-7F7|
+        |L.L7LFJ|||||FJL7||LJ
+        |L7JLJL-JLJLJL--JLJ.L""".stripMargin.split("\n").toList
+    assert(part2(lines) == 10)
   }
